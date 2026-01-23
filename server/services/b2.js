9ea40058b855efde3be6d00a1e070b42e3e20f87
TechnoGroup10 <projecttechno10@gmail.com>
2026-01-23 11:52:46 +0700

    fix migrate to b2 2

--- server/services/b2.js
+++ server/services/b2.js
@@ -1,15 +1,16 @@
 const { S3Client, PutObjectCommand, DeleteObjectCommand } = require("@aws-sdk/client-s3");
 
 const s3 = new S3Client({
-    region: "auto",
-    endpoint: process.env.B2_ENDPOINT,
+    region: process.env.B2_REGION,
+    endpoint: process.env.B2_ENDPOINT, // ← sekarang valid URL
     credentials: {
         accessKeyId: process.env.B2_KEY_ID,
         secretAccessKey: process.env.B2_APPLICATION_KEY
-    }
+    },
+    forcePathStyle: true // WAJIB untuk Backblaze B2
 });
 
-const uploadToB2 = async (buffer, key, contentType) => {
+async function uploadToB2(buffer, key, contentType) {
     await s3.send(new PutObjectCommand({
         Bucket: process.env.B2_BUCKET_NAME,
         Key: key,
@@ -17,14 +18,14 @@ const uploadToB2 = async (buffer, key, contentType) => {
         ContentType: contentType
     }));
 
-    return `${process.env.B2_PUBLIC_URL}/${key}`;
-};
+    return `${process.env.B2_ENDPOINT}/${process.env.B2_BUCKET_NAME}/${encodeURIComponent(key)}`;
+}
 
-const deleteFromB2 = async (key) => {
+async function deleteFromB2(key) {
     await s3.send(new DeleteObjectCommand({
         Bucket: process.env.B2_BUCKET_NAME,
         Key: key
     }));
-};
+}
 
 module.exports = { uploadToB2, deleteFromB2 };
